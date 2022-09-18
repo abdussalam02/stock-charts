f164176f7d2d5ed213a1e78c68b9bace9600cd46
Abdus Salam <87809119+abdussalam02@users.noreply.github.com>
2022-09-18 13:05:12 +0530

    made some changes

--- line_chart.py
+++ line_chart.py
@@ -1,19 +1,24 @@
-import pandas as pd
+# import pandas as pd
 import pandas_datareader.data as web
 import matplotlib.pyplot as plt
 import datetime
 from datetime import date, timedelta
+
 today = date.today()
 
+# Formatting todays date time
 d1 = today.strftime("%Y/%m/%d")
 end_date = d1
+
 d2 = date.today() - timedelta(days=360)
 d2 = d2.strftime("%Y/%m/%d")
 start_date = d2
+
 import streamlit as st
 st.title("Real Time Stock Price Data")
 a = st.text_input("Enter the name of Indian Stock: ")+(".ns")
 stock = a
+
 data = web.DataReader(name=a, data_source='yahoo', start=start_date, end=end_date)
 fig, ax = plt.subplots()
 ax = data["Close"].plot(figsize=(12, 8), title=a.upper(), fontsize=20, label="Close Price")
@@ -21,4 +26,5 @@ plt.legend()
 plt.grid()
 st.pyplot(fig)
 
+# converting the data to CSV format to plot it as candlestick
 data.to_csv(stock + ".csv", mode = 'a', header = True)
